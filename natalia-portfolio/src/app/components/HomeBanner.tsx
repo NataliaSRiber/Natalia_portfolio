1df2cdf80bb186e538494304581ce165bd2ad9dc
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2023-10-24 09:40:40 -0300

    feat: it removes ux banner

--- natalia-portfolio/src/app/components/HomeBanner.tsx
+++ natalia-portfolio/src/app/components/HomeBanner.tsx
@@ -48,35 +48,6 @@ export default function Homebanner() {
             Projetos de UX Design
           </h1>
         </div>
-        <div className="flex flex-wrap justify-center ">
-          {uxdesign.map(
-            ({ title, description, date, videoUrl, url }, index) => (
-              // <div key={index} className="p-4 md:w-1/2 xl:w-1/3">
-              <div key={index} className="my-4 px-2 md:my-0 md:w-1/2">
-                <a href={url} target="_blank">
-                  <div className="h-full overflow-hidden rounded-lg border-2 border-newpink-500 border-opacity-60">
-                    <Image
-                      className="h-full cursor-pointer hover:brightness-50"
-                      src={videoUrl}
-                      alt="dance-picture"
-                      width={800}
-                      height={800}
-                    />
-                    {/* <div className="h-full cursor-pointer p-6 transition duration-300 ease-in hover:bg-newpink-500 hover:text-gray-100">
-                      <h2 className="mb-1 text-sm font-medium text-blue-300 md:text-base">
-                        {date}
-                      </h2>
-                      <h1 className="mb-3 text-xl font-semibold md:text-2xl">
-                        {title}
-                      </h1>
-                      <p className="mb-3 leading-relaxed">{description}</p>
-                    </div> */}
-                  </div>
-                </a>
-              </div>
-            ),
-          )}
-        </div>
       </div>
     </section>
   )
--- natalia-portfolio/src/app/page.tsx
+++ natalia-portfolio/src/app/page.tsx
@@ -4,19 +4,23 @@ import Homebanner from './components/HomeBanner'
 
 export default function Home() {
   return (
-    <main className="mx-6	mt-24 flex flex-col items-center justify-center pb-20 lg:mx-20 lg:mt-40">
+    <main className="mx-6 mt-28 flex flex-col items-center justify-center pb-20 md:mt-40 lg:mx-20 lg:mt-24">
       <div className="flex w-full flex-col items-center justify-center pb-10 lg:flex-row lg:pb-20">
-        <div className="items-center space-y-8 text-justify md:pl-6 lg:w-1/2 lg:flex-row lg:space-y-16">
-          <p className="text-center text-2xl font-bold text-maincolor-800 md:text-4xl">
+        <div className="items-center space-y-8 text-justify md:pl-6 lg:w-1/2 lg:flex-row lg:space-y-6">
+          <p className="text-center text-2xl font-bold text-maincolor-800 md:text-left md:text-2xl">
             Olá, Bem-Vindo ao meu Portfólio!
           </p>
-          <div className="space-y-6 pb-8 lg:pb-0">
-            <h1 className="text-center text-lg font-semibold md:text-2xl">
+          <div className="space-y-6 pb-4 lg:pb-0">
+            <h1 className="text-center text-lg font-semibold md:text-left md:text-4xl">
               Meu nome é Natalia,
             </h1>
-            <h2 className="text-center text-2xl font-bold text-newpink-500 md:text-4xl">
+            <h2 className="text-center text-2xl font-semibold text-newpink-500 md:text-left md:text-4xl">
               Sou Desenvolvedora Full Stack
             </h2>
+          </div>
+          <div>
+            {/* <button>Mais informações</button> */}
+            {/* <button>Mais informações</button> */}
           </div>
         </div>
         <div className="mb-6 flex items-center justify-center lg:mb-0 lg:w-1/3">
