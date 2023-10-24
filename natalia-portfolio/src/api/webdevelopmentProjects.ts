3f1d69fbd5e988eadc707adceb953d9abc9b8c0b
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2023-10-24 09:14:08 -0300

    feat: it modifies responsive font size

--- natalia-portfolio/src/api/webdevelopmentProjects.ts
+++ natalia-portfolio/src/api/webdevelopmentProjects.ts
@@ -1,6 +1,7 @@
 import { StaticImageData } from 'next/image'
 import dancegroup from '../assets/developedProjects/dancegroup.png'
 import drinkswebsite from '../assets/developedProjects/drinkswebsite.png'
+import bakeryapp from '../assets/uxdesignprojects/backeryapp.png'
 
 export interface IPresentations {
   title: string
@@ -14,7 +15,7 @@ export interface IUxdesign {
   title: string
   description: string
   date: string
-  // url: string
+  url: string
   videoUrl: StaticImageData
 }
 
@@ -37,19 +38,29 @@ export const presentations: IPresentations[] = [
   },
 ]
 
-// export const uxdesign: IPresentations[] = [
-//   {
-//     title: 'Apresentação dia das mães',
-//     description:
-//       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at lorem et neque rhoncus sagittis vel vitae urna. Aenean feugiat orci in fermentum malesuada. Donec libero erat, bibendum eget vehicula nec, vestibulum id mauris.',
-//     date: 'October 21, 2020',
-//     videoUrl: dancegroup,
-//   },
-//   {
-//     title: 'Apresentação de Natal',
-//     description:
-//       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at lorem et neque rhoncus sagittis vel vitae urna. Aenean feugiat orci in fermentum malesuada. Donec libero erat, bibendum eget vehicula nec, vestibulum id mauris.',
-//     date: 'October 21, 2020',
-//     videoUrl: drinkswebsite,
-//   },
-// ]
+export const uxdesign: IUxdesign[] = [
+  {
+    title: 'Aplicativo da Padaria Doces Sonhos',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at lorem et neque rhoncus sagittis vel vitae urna. Aenean feugiat orci in fermentum malesuada. Donec libero erat, bibendum eget vehicula nec, vestibulum id mauris.',
+    date: 'October 21, 2020',
+    url: 'https://www.figma.com/proto/XpO1isL7UeGeLzxF39Batm/bakery_mockup(final)?type=design&node-id=516-379&t=N7DW6KJsBZwpnD5V-1&scaling=scale-down&page-id=516%3A2&starting-point-node-id=516%3A379&mode=design',
+    videoUrl: bakeryapp,
+  },
+  {
+    title: 'Apresentação de Natal',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at lorem et neque rhoncus sagittis vel vitae urna. Aenean feugiat orci in fermentum malesuada. Donec libero erat, bibendum eget vehicula nec, vestibulum id mauris.',
+    date: 'October 21, 2020',
+    url: '',
+    videoUrl: drinkswebsite,
+  },
+  {
+    title: 'Apresentação de Natal',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at lorem et neque rhoncus sagittis vel vitae urna. Aenean feugiat orci in fermentum malesuada. Donec libero erat, bibendum eget vehicula nec, vestibulum id mauris.',
+    date: 'October 21, 2020',
+    url: '',
+    videoUrl: drinkswebsite,
+  },
+]
--- natalia-portfolio/src/app/components/HomeBanner.tsx
+++ natalia-portfolio/src/app/components/HomeBanner.tsx
@@ -1,5 +1,4 @@
-import { presentations } from '@/api/webdevelopmentProjects'
-import { url } from 'inspector'
+import { presentations, uxdesign } from '@/api/webdevelopmentProjects'
 import Image from 'next/image'
 import React from 'react'
 
@@ -13,11 +12,11 @@ export default function Homebanner() {
             Projetos de Desenvolvimento Web
           </h1>
         </div>
-        <div className="flex flex-wrap justify-center ">
+        <div className="flex flex-wrap justify-center">
           {presentations.map(
             ({ title, description, date, videoUrl, url }, index) => (
               // <div key={index} className="p-4 md:w-1/2 xl:w-1/3">
-              <div key={index} className="my-4 md:my-0 md:w-1/2">
+              <div key={index} className="my-4 px-2 md:my-0 md:w-1/2">
                 <a href={url} target="_blank">
                   <div className="h-full overflow-hidden rounded-lg border-2 border-newpink-500 border-opacity-60">
                     <Image
@@ -43,40 +42,42 @@ export default function Homebanner() {
           )}
         </div>
       </div>
-      {/* <div className="container mx-auto w-full pb-16">
+      <div className="container mx-auto w-full pb-16">
         <div className="pb-10 text-center">
-          <h1 className="px-2 text-xl font-semibold text-gray-700 md:text-5xl">
+          <h1 className="px-2 text-xl font-semibold text-maincolor-800 md:text-3xl">
             Projetos de UX Design
           </h1>
         </div>
         <div className="flex flex-wrap justify-center ">
-          {presentations.map(
-            ({ title, description, date, videoUrl }, index) => (
+          {uxdesign.map(
+            ({ title, description, date, videoUrl, url }, index) => (
               // <div key={index} className="p-4 md:w-1/2 xl:w-1/3">
-              <div key={index} className="p-4 md:w-1/2">
-                <div className="h-full overflow-hidden rounded-lg border-2 border-newpink-500 border-opacity-60">
-                  <Image
-                    className="h-full cursor-pointer hover:brightness-50"
-                    src={videoUrl}
-                    alt="dance-picture"
-                    width={800}
-                    height={800}
-                  />
-                  <div className="h-full cursor-pointer p-6 transition duration-300 ease-in hover:bg-newpink-500 hover:text-gray-100">
-                    <h2 className="mb-1 text-sm font-medium text-blue-300 md:text-base">
-                      {date}
-                    </h2>
-                    <h1 className="mb-3 text-xl font-semibold md:text-2xl">
-                      {title}
-                    </h1>
-                    <p className="mb-3 leading-relaxed">{description}</p>
+              <div key={index} className="my-4 px-2 md:my-0 md:w-1/2">
+                <a href={url} target="_blank">
+                  <div className="h-full overflow-hidden rounded-lg border-2 border-newpink-500 border-opacity-60">
+                    <Image
+                      className="h-full cursor-pointer hover:brightness-50"
+                      src={videoUrl}
+                      alt="dance-picture"
+                      width={800}
+                      height={800}
+                    />
+                    {/* <div className="h-full cursor-pointer p-6 transition duration-300 ease-in hover:bg-newpink-500 hover:text-gray-100">
+                      <h2 className="mb-1 text-sm font-medium text-blue-300 md:text-base">
+                        {date}
+                      </h2>
+                      <h1 className="mb-3 text-xl font-semibold md:text-2xl">
+                        {title}
+                      </h1>
+                      <p className="mb-3 leading-relaxed">{description}</p>
+                    </div> */}
                   </div>
-                </div>
+                </a>
               </div>
             ),
           )}
         </div>
-      </div> */}
+      </div>
     </section>
   )
 }
