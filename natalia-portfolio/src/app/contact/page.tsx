a68e65c9c0532b476c52497e68a5847b545a9b35
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2023-11-30 16:40:50 -0300

    feat: it adds some contact information

--- natalia-portfolio/src/app/contact/page.tsx
+++ natalia-portfolio/src/app/contact/page.tsx
@@ -1,25 +1,25 @@
 export default function Contact() {
   return (
-    <div className="mx-10 mb-28 mt-40 flex flex-col gap-20">
+    <div className="mx-10 mb-28 mt-40 flex flex-col gap-10">
       <h1 className="mb-6 text-center text-2xl font-bold text-maincolor-800 md:text-5xl lg:mb-10">
         Contato
       </h1>
-      <p className="font-regular text-xl">Email: nataliasribeiro15@gmail.com</p>
-      {/* <p className="font-regular cursor-pointer text-xl">
-        <a
-          href="https://dance-group-website-template.vercel.app/"
-          target="_blank"
-        ></a>
-        Currículo
-      </p> */}
-      {/* <h1 className="mb-6 text-center text-lg font-bold text-maincolor-800 md:text-5xl lg:mb-10">
-        Telefone
-      </h1>
-      <p className="font-regular text-xl">(35) 98405-0194</p> */}
-      <h1 className="mb-6 text-center text-2xl font-bold text-maincolor-800 md:text-5xl lg:mb-10">
-        Redes Sociais
-      </h1>
-      {/* <p className="font-regular text-xl">Linkedin e binance se tiver</p> */}
+      <div>
+        <h2 className=" mb-3 text-center text-lg font-semibold text-newpink-500 md:text-left md:text-xl">
+          Email
+        </h2>
+        <p>nataliasribeiro15@gmail.com</p>
+      </div>
+      <div>
+        <h2 className="text-center text-lg font-semibold text-newpink-500 md:text-left md:text-xl">
+          <a
+            href="https://www.linkedin.com/in/natalia-de-souza-ribeiro/"
+            target="_blank"
+          >
+            Linkedin
+          </a>
+        </h2>
+      </div>
     </div>
   )
 }
